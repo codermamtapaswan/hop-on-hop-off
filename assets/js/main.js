7b12a7d31b1c0a8ffcc3b0cc8f670630d79e13c1
mamtabattersea24 <dev.battersea@gmail.com>
2024-10-16 15:20:36 +0530

    new upadate

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,31 @@
 
 document.addEventListener("DOMContentLoaded", function () {
   var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 4,
-    spaceBetween: 30,
-    loop: true,
+    spaceBetween: 20,
+    loop:true,
+    autoplay:true,
+    centeredSlides: true,
+    grabCursor: true,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      
+      },
+      480: {
+        slidesPerView: 2,
+      
+      },
+      769: {
+        slidesPerView: 4,
+      }
+    }
   });
+
+
   var swiper1 = new Swiper(".mySwiper1", {
     slidesPerView: 4,
     spaceBetween: 30,
@@ -17,7 +34,21 @@ document.addEventListener("DOMContentLoaded", function () {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      
+      },
+      480: {
+        slidesPerView: 2,
+      
+      },
+      769: {
+        slidesPerView: 4,
+      }
+    }
   });
+  
   var swiper2 = new Swiper(".mySwiper2", {
     slidesPerView: 4,
     spaceBetween: 30,
@@ -26,7 +57,44 @@ document.addEventListener("DOMContentLoaded", function () {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      
+      },
+      480: {
+        slidesPerView: 2,
+      
+      },
+      769: {
+        slidesPerView: 4,
+      }
+    }
   });
+
+  var swiper3 = new Swiper(".mySwiper3", {
+    slidesPerView: 3,
+    autoplay:true,
+    spaceBetween: 30,
+    loop: true,
+    centeredSlides: true,
+    roundLengths: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      
+      },
+      480: {
+        slidesPerView: 2,
+      
+      },
+      769: {
+        slidesPerView: 3,
+      }
+    }
+  });
+
+
 });
 
 
